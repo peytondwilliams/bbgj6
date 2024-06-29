91fa8879831e5107f897590908dc4f41e57bdccd
Peyton Williams <peytonwilliams5@gmail.com>
2024-06-29 09:22:20 -0400

    wip dimension logic

--- scenes/agents/Player.gd
+++ scenes/agents/Player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var eb = EventBus
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
---
+++ scenes/objects/Portal.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+var to_dimension := 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_body_entered(body):
+	if body.has_method("swap_dimension"):
+		body.swap_dimension(to_dimension)
